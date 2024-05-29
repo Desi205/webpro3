2c76d268874b375941ce55715e6faee025824ab6
Desi205 <desiwlnwulan@gmail.com>
2024-05-29 18:46:21 +0700

    pertemuan9

---
+++ application/views/pinjam/laporan-pdf-pinjam.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<style type="text/css">
+.table-data{
+ width: 100%;
+ border-collapse: collapse;
+ }
+ .table-data tr th,
+ .table-data tr td{
+ border:1px solid black;
+ font-size: 11pt;
+ font-family:Verdana;
+ padding: 10px 10px 10px 10px;
+ }
+ .table-data th{
+ background-color:grey;
+ }
+ h3{
+ font-family:Verdana;
+ }
+</style>
+<h3><center>LAPORAN DATA PEMINJAMAN BUKU</center></h3>
+<br/>
+<table class="table-data">
+ <thead>
+ <tr>
+ <th>No</th>
+ <th>Nama Anggota</th>
+ <th>Judul Buku</th>
